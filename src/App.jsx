1f799ca66a421b7059ab9b00f851506e88f19622
flaviabianco <flaviadayanabianco@gmail.com>
2025-03-10 02:13:16 +0000

    ajustes rotas

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 
 const router = createBrowserRouter ([
-  {path: '/', element: <PhotoAlbum/>},
+  {path: '/portfolio-album-photography', element: <PhotoAlbum/>},
   {path:'/album', element: <Album/>},
  
 ])
